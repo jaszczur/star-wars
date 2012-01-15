2c77f4b4ef225997e3499bf1c237c969222623da
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2012-01-15 14:53:02 +0100

    code refactor

--- starwars.js
+++ starwars.js
@@ -1,5 +1,31 @@
 var StarWars = {};
 
+StarWars.trueHeight = 0;
+
+StarWars.initialize = function() {
+  StarWars.trueHeight = StarWars.measureTrueHeight();
+  StarWars.setupAudio();
+}
+
+StarWars.measureTrueHeight = function() {
+  var tempObj = $('#textContainer') // starting with truncated text div container
+      .clone().contents() // duplicate contents
+      .wrapAll('<div id="content"/>') // wrap them in a container
+      .parent().appendTo('#scene') // add this to the dom
+      .css('top','-1000px'); // but put it far off-screen
+  var result = tempObj.height(); // measure it
+  tempObj.remove(); // clean up
+  return result;
+}
+
+StarWars.setupAudio = function() {
+  var audio = document.getElementById("song");
+  audio.addEventListener("playing", function() {
+    StarWars.startAnimation();
+  });
+  audio.play();
+}
+
 StarWars.startAnimation = function() {
   var shift_distance = 2 * StarWars.trueHeight - $("#textContainer").height(); // how far to move
   var time_normalized = parseInt(shift_distance / 100, 10) * 2000; // speed
@@ -9,28 +35,9 @@ StarWars.startAnimation = function() {
     }, time_normalized, 'linear'); // and move the div within its "viewport"
 }
 
-//StarWars.
-
-StarWars.trueHeight = 0;
 
 $(document).ready(function() {
-  StarWars.trueHeight = ( function(){
-    var $tempobj = $('#textContainer') // starting with truncated text div container
-      .clone().contents() // duplicate the text
-      .wrapAll('<div id="content"/>') // wrap it in a container
-      .parent().appendTo('#scene') // add this to the dom
-      .css('top','-1000px'); // but put it far off-screen
-    var result = $tempobj.height(); // measure it
-    $tempobj.remove(); // clean up
-    return result;
-  })();
-  
-  var audio = document.getElementById("song");
-  audio.addEventListener("playing", function() {
-    StarWars.startAnimation();
-  });
-  audio.play();
-  
+  StarWars.initialize();
 });
 
 
