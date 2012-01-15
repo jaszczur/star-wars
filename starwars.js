185d5c5840139664481a48bfcf5d94765ab70602
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2012-01-15 14:05:36 +0100

    windows working version

--- starwars.js
+++ starwars.js
@@ -1,22 +1,36 @@
+var StarWars = {};
+
+StarWars.startAnimation = function() {
+  var shift_distance = 2 * StarWars.trueHeight - $("#textContainer").height(); // how far to move
+  var time_normalized = parseInt(shift_distance / 100, 10) * 2000; // speed
+  $("#textContainer").contents().wrapAll('<div id="content">').parent() // wrap in div
+    .animate({
+      top: -shift_distance
+    }, time_normalized, 'linear'); // and move the div within its "viewport"
+}
+
+//StarWars.
+
+StarWars.trueHeight = 0;
 
 $(document).ready(function() {
-  var true_height = ( function(){
+  StarWars.trueHeight = ( function(){
     var $tempobj = $('#textContainer') // starting with truncated text div container
       .clone().contents() // duplicate the text
-      .wrap('<div id="content"/>') // wrap it in a container
+      .wrapAll('<div id="content"/>') // wrap it in a container
       .parent().appendTo('#scene') // add this to the dom
       .css('top','-1000px'); // but put it far off-screen
     var result = $tempobj.height(); // measure it
     $tempobj.remove(); // clean up
     return result;
   })();
-
-    var shift_distance = 2 * true_height - $("#textContainer").height(); // how far to move
-    var time_normalized = parseInt(shift_distance / 100, 10) * 1000; // speed
-    $("#textContainer").contents().wrap('<div id="content">').parent() // wrap in div
-      .animate({
-        top: -shift_distance
-      }, time_normalized, 'linear'); // and move the div within its "viewport"
+  
+  var audio = $("#song").get(0);
+  audio.play();
+  StarWars.startAnimation();
+  
+  
+  
 });
 
 
