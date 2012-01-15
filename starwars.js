44d500213a11989147a24d8447191ffe8ba817bb
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2012-01-15 11:11:57 +0100

    animation

---
+++ starwars.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+  console.log("di");
+  var true_height = ( function(){
+    var $tempobj = $('#textContainer') // starting with truncated text div container
+      .clone().contents() // duplicate the text
+      .wrap('<div id="content"/>') // wrap it in a container
+      .parent().appendTo('#scene') // add this to the dom
+      .css('top','-1000px'); // but put it far off-screen
+    var result = $tempobj.height(); // measure it
+    $tempobj.remove(); // clean up
+    return result;
+  })();
+
+    var shift_distance = true_height - $("#textContainer").height(); // how far to move
+    var time_normalized = parseInt(shift_distance / 100, 10) * 5000; // speed
+    $("#textContainer").contents().wrap('<div id="content">').parent() // wrap in div
+      .animate({
+        top: -shift_distance
+      }, time_normalized, 'linear'); // and move the div within its "viewport"
+});
+
+
